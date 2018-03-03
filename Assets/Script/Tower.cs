543a1c7cc3db47e533cd13348cfff82b39c5099c
Matriac <CharlesGagnon123@hotmail.com>
2018-03-03 02:21:54 -0500

    Add building cs

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -4,30 +4,17 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
-	public enum TowerState{
-		inGame = 1,
-		inConstruction = 2
-	}
 
-	TowerState state;
     public int health;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	public void changeState(TowerState newState){
-		state = newState;
-		if (newState == TowerState.inConstruction) {
-			GetComponent<SpriteRenderer> ().color = Color.green;
-		} else
-			GetComponent<SpriteRenderer> ().color = Color.white;
 
-	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.I))
-			changeState (TowerState.inConstruction);
+
 	}
 
     public void Hurt(int amount)
