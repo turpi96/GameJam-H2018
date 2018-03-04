ff6b072eafa10104ea0692757a7185e83b831765
patsimd <>
2018-03-04 01:26:35 -0500

    MapDamage Spell

---
+++ Assets/Script/MapDamageThing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDamageThing : MonoBehaviour
+{
+
+    public Sprite explosion;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Explode());
+        Destroy(gameObject, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<SpriteRenderer>().sprite = explosion;
+    }
+}
