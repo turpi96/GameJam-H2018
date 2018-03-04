e88ab17ee2181ef880aa0dbf08568fa1b6233b91
Matriac <CharlesGagnon123@hotmail.com>
2018-03-04 11:07:42 -0500

    Add LoadingMap

---
+++ Assets/Script/LoadingMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingMap : MonoBehaviour {
+
+	public float timeToWait = 3.87f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeToWait -= Time.deltaTime;
+		if (timeToWait <= 0.0f) {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+}
